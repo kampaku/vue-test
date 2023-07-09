3d135841bc890dd37862e19b0240a32f9251b809
kampaku <kampaku@users.noreply.github.com>
2023-07-09 16:35:58 +0300

    первоначальная настройка

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header">
+    <div class="logo"><img src="../assets/images/logo.svg" alt="logo" /></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './assets/fonts/font.scss'
+import './style.scss'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
