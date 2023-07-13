beca2cc10514b9c744df1271389b92a871a4d3b9
kampaku <43931529+kampaku@users.noreply.github.com>
2023-07-13 20:26:13 +0300

    сделал модалку

---
+++ src/components/CallbackForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="callback">
+    <div class="callback__col">
+      <h2 class="callback__title">Оставьте заявку</h2>
+      <p class="callback__subtitle">Скоро с вами свяжется администратор</p>
+      <p class="callback__policy">
+        Нажимая на кнопку «Отправить», Вы соглашаетесь с <a href="/">Политикой конфиденциальности</a>
+      </p>
+    </div>
+    <div class="callback__col">
+      <form class="callback__form" @submit.prevent="onSubmit">
+        <custom-input label="Ваше имя" v-model="name" required></custom-input>
+        <custom-input label="Ваш номер телефона" v-model="tel" mask="+7(###)###-##-##" required></custom-input>
+        <custom-button>Забронировать</custom-button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CustomInput from './CustomInput.vue';
+const name = ref('')
+const tel = ref('')
+const emit = defineEmits(['close'])
+const onSubmit = () => emit('close')
+</script>
+
+<style lang="scss" scoped>
+.callback {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  place-content: center;
+  gap: 20px;
+  max-width: 702px;
+
+  &__col {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__title {
+    margin: 0;
+    font-family: P052;
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 32px;
+    color: var(--color-primary);
+  }
+
+  &__subtitle {
+    font-size: 16px;
+    line-height: 28px;
+    color: var(--color-grey);
+  }
+
+  &__policy {
+    margin-top: auto;
+    font-family: Roboto;
+    font-size: 12px;
+    line-height: 15px;
+    color: var(--color-grey);
+
+    a {
+      color: inherit;
+    }
+  }
+
+  &__form {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+}
+</style>
---
+++ src/components/ModalDialog.vue
@@ -0,0 +1,77 @@
+<template>
+  <Transition name="modal">
+    <div v-if="show" class="modal" @click="onClose">
+      <div @click.stop class="modal__container">
+        <div class="modal__header">
+          <button class="modal__close" @click="onClose">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                fill="#18191B"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div class="modal__body">
+          <slot>default body</slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+const props = defineProps({
+  show: Boolean,
+})
+
+const emit = defineEmits(['close'])
+
+const onClose = () => {
+  emit('close')
+}
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(24, 25, 27, 0.9);
+
+  &__container {
+    min-height: 100px;
+    min-width: 300px;
+    border-radius: var(--border-radius);
+    background-color: var(--color-white);
+  }
+
+  &__header {
+    display: flex;
+    padding: 14px 14px 0 0;
+  }
+
+  &__close {
+    margin-left: auto;
+    border: 0;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  &__body {
+    padding: 0px 80px 60px 40px;
+  }
+}
+</style>
